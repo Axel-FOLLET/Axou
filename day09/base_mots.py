aaa3c7371913c56cfa9df3c856905c93ffcde781
Axou <axel.follet@epitech.eu>
2026-09-17 16:34:21 +0200

    maj

---
+++ day09/base_mots.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import random
+
+def choisir_mot():
+    # ouvre le dictionnaire français et lit tous les mots, un par ligne
+    chemin_fichier = "day09/mots_simples.txt"
+    with open(chemin_fichier) as fichier:
+        mots = fichier.readlines()
+    if len(mots) == 0:
+        # sécurité si le fichier est vide, pour éviter un plantage lors du choix aléatoire
+        print("Error: empty file")
+        exit()
+    return random.choice(mots).strip()
